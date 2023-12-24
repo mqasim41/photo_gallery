b9839391d25a3326668f8feeea50883126316db1
attiya-waqar <awaqar.bese21seecs@seecs.edu.pk>
2023-12-24 15:19:10 +0500

    minor changes

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,26 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
 export const Gallery = (props) => {
-    const [images, setImages] = useState([
-    'https://images.unsplash.com/photo-1532012197267-da84d127e765?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1532012197267-da84d127e765?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1532012197267-da84d127e765?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1532012197267-da84d127e765?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1532012197267-da84d127e765?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    // Add more image URLs as needed
-  ]);
+  //   const [images, setImages] = useState([
+  //   'https://images.unsplash.com/photo-1532012197267-da84d127e765?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
+  //   'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  //   'https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  //   'https://images.unsplash.com/photo-1532012197267-da84d127e765?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
+  //   'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  //   // Add more image URLs as needed
+  // ]);
+  const [images, setImages] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
-   const [selectedImages, setSelectedImages] = useState([]);
+  const [selectedImages, setSelectedImages] = useState([]);
 
     const toggleImageSelection = (imageUrl) => 
     {
@@ -33,28 +24,95 @@ export const Gallery = (props) => {
 	    }
  	};
 
- 	  const deleteSelectedImages = () => {
-    const updatedImages = images.filter(
-      (imageUrl) => !selectedImages.includes(imageUrl)
-    );
-    setImages(updatedImages);
-    setSelectedImages([]);
+ 	const deleteSelectedImages = () => 
+ 	{
+    	const updatedImages = images.filter(
+      	(imageUrl) => !selectedImages.includes(imageUrl)
+    	);
+	    setImages(updatedImages);
+	    setSelectedImages([]);
+ 	 };
+
+   const uploadImages = async (files) =>
+   {
+    try 
+    {
+    const formData = new FormData();
+
+    for (let i = 0; i < files.length; i++) 
+    {
+      formData.append('images', files[i]);
+    }
+      const response = await fetch('http://104.198.137.113:6869/upload', 
+      {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) 
+      {
+        // If the images were uploaded successfully, update the image gallery
+        // Fetch the updated image URLs from the server or update as needed
+        // For this example, assuming the response includes new image URLs
+        const { uploadedImages } = await response.json();
+        setImages([...images, ...uploadedImages]);
+      } 
+      else 
+      {
+        console.error('Failed to upload images', response);
+      }
+    } 
+    catch (error) 
+    {
+      console.error('Error uploading images:', error);
+    }
+   };
+
+  const uploadImagesToServer = async (files) => 
+  {
+    const response = await fetch(`${BASE_URL}/upload`, 
+    {
+      method: 'POST',
+      body: formData,
+      headers: 
+      {
+        Authorization: `Bearer YOUR_ACCESS_TOKEN`, // Replace with your actual access token
+      },
+    });
+
+      if (response.ok) {
+        // If the images were uploaded successfully, update the image gallery
+        // Fetch the updated image URLs from the server or update as needed
+        // For this example, assuming the response includes new image URLs
+        const { uploadedImages } = await response.json();
+        setImages([...images, ...uploadedImages]);
+      } else {
+        console.error('Failed to upload images');
+      }
+    } catch (error) {
+      console.error('Error uploading images:', error);
+    }
   };
 
-   const handleFileSelect = (event) => {
+   const handleFileSelect = (event) => 
+   {
     const selectedFiles = event.target.files;
     const uploadedImages = Array.from(selectedFiles).map((file) =>
       URL.createObjectURL(file)
     );
     setImages([...images, ...uploadedImages]);
+    uploadImages(selectedFiles);
   };
 
-  const openImageEditor = (imageUrl) => {
-  if (imageUrl) {
+  const openImageEditor = (imageUrl) =>
+  {
+  if (imageUrl) 
+  {
     const img = new Image();
     img.src = imageUrl;
 
-    img.onload = () => {
+    img.onload = () => 
+    {
       const maxWidth = window.innerWidth * 0.9; // Set the maximum width as 90% of window width
       const maxHeight = window.innerHeight * 0.9; // Set the maximum height as 90% of window height
 
@@ -78,7 +136,8 @@ export const Gallery = (props) => {
 
       const editorWindow = window.open('', 'Image Editor', `width=${newWidth},height=${newHeight},left=${left},top=${top}`);
 
-      if (editorWindow) {
+      if (editorWindow) 
+      {
         editorWindow.document.head.innerHTML = `
           <style>
             /* Existing styles remain unchanged */
@@ -106,7 +165,6 @@ export const Gallery = (props) => {
         editorWindow.document.body.innerHTML = `
           <div class="editor-content">
             <h2>Edit Image</h2>
-            <h2>Delete</h2>
             <img src="${imageUrl}" alt="Selected" style="width: ${newWidth}px; height: ${newHeight}px;">
             <button class="edit-btn">Edit</button>
           </div>
